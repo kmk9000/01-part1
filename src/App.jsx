45684ae008723aadc42c82399379d2af5fc25f9a
kmk9000 <kallekoivuniemi@gmail.com>
2025-08-15 09:34:25 +0300

    up to refractor exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-const App = () => {
-  const now = newDate();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
-  console.log("Hello from component");
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
   return (
     <>
-      <p>Hello World, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
       </p>
     </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
   );
 };
 
