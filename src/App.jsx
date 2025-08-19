b8a86ca7090c95bd9035852aa38f0e643565f9f9
kmk9000 <kallekoivuniemi@gmail.com>
2025-08-19 14:00:28 +0300

    next step

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
   const now = newDate();
   const a = 10;
@@ -12,4 +23,17 @@ const App = () => {
       </p>
     </>
   );
+  const name = "Pekka";
+  const age = 10;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  );
 };
+
+export default App;
